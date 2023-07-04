ff035c0b2b31db21dc1904573e876a3ac8bc7b73
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-07-04 14:28:12 -0500

    hmm feels exp

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -2,12 +2,11 @@ import { createServer } from "node:http";
 import { Plugin, createYoga } from "graphql-yoga";
 import { buildHTTPExecutor } from "@graphql-tools/executor-http";
 import { useExecutor } from "@graphql-tools/executor-yoga";
+import { createRouter } from "fets";
 
 const endpoint = "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
 
-const remoteExecutor = buildHTTPExecutor({
-  endpoint,
-});
+const router = createRouter();
 
 /**
  * It is a safe assumption that we skip validation on this gateway
@@ -20,13 +19,46 @@ const skipValidate: Plugin = {
   },
 };
 
-const yoga = createYoga({
-  plugins: [skipValidate, useExecutor(remoteExecutor)],
-  parserAndValidationCache: true,
-  maskedErrors: false,
+const yoga = ({ endpoint }: { endpoint: string }) => {
+  const remoteExecutor = buildHTTPExecutor({
+    endpoint,
+  });
+
+  return createYoga({
+    plugins: [skipValidate, useExecutor(remoteExecutor)],
+    parserAndValidationCache: true,
+    maskedErrors: false,
+    landingPage: false,
+    graphqlEndpoint: "/",
+  });
+};
+
+router.route({
+  path: "/:username/:subgraph_name/*",
+  schemas: {
+    request: {
+      params: {
+        type: "object",
+        properties: {
+          username: { type: "string" },
+          subgraph_name: { type: "string" },
+        },
+        additionalProperties: false,
+        required: ["username", "subgraph_name"],
+      },
+    },
+  } as const,
+  handler: async (req, res) => {
+    const { username, subgraph_name } = req.params;
+
+    const yo = yoga({
+      endpoint: `https://api.thegraph.com/subgraphs/name/${username}/${subgraph_name}`,
+    });
+    return yo.handle(req);
+  },
 });
 
-const server = createServer(yoga);
+const server = createServer(router.handle);
 
 server.listen(4000, () => {
   console.log("Server is listening on port 4000");

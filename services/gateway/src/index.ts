14a42e12c6332ad5d37b7c97230e2b2499025388
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2023-07-24 16:08:11 -0500

    sorta works

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -1,12 +1,7 @@
 import { createServer } from "node:http";
 import { Plugin, createLRUCache, createYoga } from "graphql-yoga";
-import { createRouter } from "fets";
-import { hashOperation, normalizeOperation } from "@graphql-hive/core";
+import { normalizeOperation } from "@graphql-hive/core";
 import { createHash } from "crypto";
-
-const endpoint = "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
-
-const router = createRouter();
 
 /**
  * It is a safe assumption that we skip validation on this gateway
@@ -27,110 +22,106 @@ const cache = createLRUCache<{
 }>();
 
 function createHashKey({
-  hash,
   endpoint,
   variables,
+  normalizedOp,
 }: {
-  hash: string;
+  normalizedOp: string;
   endpoint: string;
   variables: string;
 }) {
   return createHash("md5")
-    .update(`${hash}--${endpoint}--${variables}`)
+    .update(endpoint, "utf8")
+    .update(variables, "utf8")
+    .update(normalizedOp, "utf8")
     .digest("hex");
 }
 
-const remoteExecutor = ({ endpoint }: { endpoint: string }): Plugin => {
-  return {
-    async onExecute({ args, setResultAndStopExecution }) {
-      const normalizedOp = normalizeOperation({
-        document: args.document,
-        operationName: args.operationName,
-        removeAliases: true,
-        hideLiterals: true,
-      });
-      const variables = args.variableValues;
-      const hash = hashOperation(normalizedOp);
-      const cacheKey = createHashKey({
-        hash,
-        endpoint,
-        variables: JSON.stringify(variables),
-      });
-      const cachedData = cache.get(cacheKey);
-
-      if (cachedData) {
-        return setResultAndStopExecution(
-          cachedData.data as any // I trust me.
-        );
-      }
-
-      const res = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: normalizedOp,
-          variables,
-        }),
-      });
-      const data = await res.json();
-
-      cache.set(cacheKey, {
-        endpoint,
-        operation: normalizedOp,
-        data,
-        variables,
-      });
-
-      console.log({
-        cacheKey,
-        query: normalizedOp,
-        hash,
-        endpoint,
-      });
-      return setResultAndStopExecution(data);
-    },
-  };
-};
-
-const yoga = ({ endpoint }: { endpoint: string }) => {
-  return createYoga({
-    plugins: [skipValidate, remoteExecutor({ endpoint })],
-    parserAndValidationCache: true,
-    maskedErrors: false,
-    landingPage: false,
-    graphqlEndpoint: "/",
-  });
-};
-
-router.route({
-  path: "/:username/:subgraph_name/*",
-  schemas: {
-    request: {
-      params: {
-        type: "object",
-        properties: {
-          username: { type: "string" },
-          subgraph_name: { type: "string" },
-        },
-        additionalProperties: false,
-        required: ["username", "subgraph_name"],
-      },
-    },
-  } as const,
-  handler: async (req, res) => {
-    const { username, subgraph_name } = req.params;
-
-    const yo = yoga({
-      endpoint: `https://api.thegraph.com/subgraphs/name/${username}/${subgraph_name}`,
-    });
-
-    return yo.handle(req);
-  },
+const hostedServicePattern = new URLPattern({
+  pathname: "/:username/:subgraph_name/graphql",
 });
 
-const server = createServer(router.handle);
+const remoteExecutor: Plugin = {
+  async onExecute({ args, setResultAndStopExecution, setExecuteFn }) {
+    const result = hostedServicePattern.exec(args.contextValue.request.url);
+    const { username, subgraph_name } = result?.pathname.groups ?? {};
+
+    if (!username || !subgraph_name) {
+      console.error({ username, subgraph_name }, "Invalid subgraph URL");
+      throw new Error("Invalid subgraph URL");
+    }
+
+    console.debug({
+      username,
+      subgraph_name,
+    });
+
+    const endpoint = `https://api.thegraph.com/subgraphs/name/${username}/${subgraph_name}`;
+
+    const normalizedOp = normalizeOperation({
+      document: args.document,
+      operationName: args.operationName,
+      removeAliases: false,
+      hideLiterals: true,
+    });
+
+    const variables = args.variableValues;
+    const cacheKey = createHashKey({
+      normalizedOp,
+      endpoint,
+      variables: JSON.stringify(variables),
+    });
+
+    console.log({
+      cacheKey,
+    });
+
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+      return setResultAndStopExecution(
+        cachedData.data as any // I trust me.
+      );
+    }
+
+    const res = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: normalizedOp,
+        variables,
+      }),
+    });
+    const data = await res.json();
+
+    cache.set(cacheKey, {
+      endpoint,
+      operation: normalizedOp,
+      data,
+      variables,
+    });
+
+    console.log({
+      cacheKey,
+      query: normalizedOp,
+      endpoint,
+    });
+
+    return setResultAndStopExecution(data);
+  },
+};
+
+const yoga = createYoga({
+  plugins: [skipValidate, remoteExecutor],
+  parserAndValidationCache: true,
+  maskedErrors: false,
+  landingPage: false,
+  graphqlEndpoint: "/:username/:subgraph_name/graphql",
+});
+
+const server = createServer(yoga);
 
 server.listen(4000, () => {
   console.log("Server is listening on port 4000");
